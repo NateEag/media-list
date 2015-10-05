eb51f9087a1da75b09f0da09da40ef34891e9573
Nate Eagleson <nate@nateeag.com>
2015-10-05 16:39:10 -0400

    Reformat and expand comments on database design

--- schema.sql
+++ schema.sql
@@ -6,15 +6,22 @@ CREATE TABLE people (
 );
 -- TODO See if PostGres supports an inline comment syntax a la MySQL.
 -- I don't love much about MySQL but that was convenient.
-COMMENT ON TABLE people 'Humans whose creative output might be interesting.';
-COMMENT ON COLUMN people.name 'Full name, exactly as entered.';
+COMMENT ON TABLE people
+'Humans whose creative output might be interesting.';
+
+COMMENT ON COLUMN people.name
+'Full name, exactly as entered.';
 
 CREATE TABLE media (
     id SERIAL PRIMARY KEY,
     name VARCHAR(200) NOT NULL
 );
-COMMENT ON TABLE media 'List of creative media we''re interested in.';
-COMMENT ON COLUMN media.name 'This medium''s name - e.g., ''Book''.';
+
+COMMENT ON TABLE media
+'List of creative media we''re interested in.';
+
+COMMENT ON COLUMN media.name
+'This medium''s name - e.g., ''Book''.';
 
 -- Initial values for media types. Users can always add more, but this is the
 -- default set.
@@ -40,14 +47,19 @@ CREATE TABLE pieces (
 
 -- TODO Look for PostGres flycheck syntax checker.
 COMMENT ON TABLE pieces
-    'Creative works we''re interested in.';
+'Creative works we''re interested in.';
+
 COMMENT ON COLUMN pieces.name
-    'This piece''s name. Should arguably be nullable, so users can enter ' ||
-    'pieces whose titles they do not know, as long as a description is provided.';
+'This piece''s name.
+
+Should arguably be nullable, so users can enter pieces whose titles they do ' ||
+'not know, as long as a description is provided.';
+
 COMMENT ON COLUMN pieces.subtitle
-    'This piece''s (optional) subtitle.';
+'This piece''s (optional) subtitle.';
+
 COMMENT ON COLUMN pieces.description
-    'Notes on the piece, perhaps including why it is of interest to the user.';
+'Notes on the piece, perhaps including why it is of interest to the user.';
 
 
 CREATE TABLE makers (
@@ -60,8 +72,11 @@ CREATE TABLE makers (
     title VARCHAR(100) NOT NULL
 );
 
-COMMENT ON TABLE makers 'Links people to creative works they contributed to.';
-COMMENT ON COLUMN makers.title 'Job title for the maker''s work on the piece.';
+COMMENT ON TABLE makers
+'Links people to creative works they contributed to.';
+
+COMMENT ON COLUMN makers.title
+'Job title for the maker''s work on the piece.';
 
 -- Record of the times a piece has been experienced.
 -- No row in this table implies that it has not yet been experienced.
@@ -74,21 +89,37 @@ CREATE TABLE experiences (
     piece_id INTEGER NOT NULL REFERENCES pieces (id),
 
     -- TODO Should the start/end fields be a single tstzrange field?
-    -- Would that make both ends required? Or is requiring both too
-    -- restrictive?
-    --
+    -- Would that make both ends required, and if so is that too restrictive?
+
     -- TODO Is there a cleaner way to make the time fields optional?
     -- Declaring them separately from the date feels odd, but with timestamp
     -- and friends, it seems like you couldn't know if the time portion was
-    -- actually exact, entered sloppily, or an indication that time is unknown.
-    date_started DATE,
-    time_started TIME,
+    -- actually exact, entered sloppily, or an attempt to indicate that time is
+    -- unknown.
+    start_date DATE,
+    start_time TIME,
 
-    date_ended DATE,
-    time_ended TIME,
+    end_date DATE,
+    end_time TIME,
 
     date_error_margin INTERVAL
 );
 
+COMMENT ON TABLE experiences
+'A record of how often pieces have been experienced.
+
+If this table has no rows for a piece, it has not been experienced.';
+
+COMMENT ON COLUMN experiences.start_date
+'The optional date the experience of the piece began.
+
+Some kinds of art can take days, weeks, or even months to get through, so the
+start date and the end date may vary widely.
+
+Books are the most likely offenders.';
+
 COMMENT ON COLUMN experiences.date_error_margin
-    'An optional record of the accuracy of the start/end fields.'
+'An optional record of the accuracy of the start/end fields.
+
+People do not always know exactly when something happened, so we let them say' ||
+'roughly how accurate they think the dates and times are.';
